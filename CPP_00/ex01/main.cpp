f598a39faeba8e26c721c2565b36d97de8509412
ruiznaomy13 <ruiznaomy13@gmail.com>
2024-02-13 15:08:05 +0100

    inicios

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncastell <ncastell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/25 18:29:23 by ncastell          #+#    #+#             */
-/*   Updated: 2024/02/13 15:01:35 by ncastell         ###   ########.fr       */
+/*   Created: 2024/02/13 14:51:09 by ncastell          #+#    #+#             */
+/*   Updated: 2024/02/13 15:07:41 by ncastell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONE_BOOK_H
-# define PHONE_BOOK_H
+#include "PhoneBook.hpp"
 
-# include <iostream>
+int	main()
+{
+	std::string	action;
 
-class PhoneBook {
-  public:
-    PhoneBook(void);
-	~PhoneBook(void);
-};
+	std::cout << "What would you wanna do?\n- SEARCH\n" << std::endl;
+	std::cin >> action;
 
-#endif
+	if (action == "SEARCH")
+	{
+		std::cout << "Give me the first name: " << std::endl;
+	}
+}
