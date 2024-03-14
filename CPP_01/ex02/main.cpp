c5eda4c969d333bf91d35f9ead353a3f199b604b
ruiznaomy13 <ruiznaomy13@gmail.com>
2024-03-14 14:20:42 +0100

    CPP01 ex02

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncastell <ncastell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 14:07:47 by ncastell          #+#    #+#             */
+/*   Updated: 2024/03/14 14:19:45 by ncastell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main()
+{
+	std::string	msg = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &msg;
+	std::string	&stringREF = msg;
+
+	std::cout << "------- MEMORY ADRESSES -------" << std::endl;
+	std::cout << "string    = " << &msg \
+	<< "\nstringPTR = " << &stringPTR \
+	<< "\nstringREF = " << &stringREF << std::endl;
+
+	std::cout << "\n------------ VALUE ------------" << std::endl;
+	std::cout << "string    = " << msg \
+	<< "\nstringPTR = " << stringPTR \
+	<< "\nstringREF = " << stringREF << std::endl;
+}
