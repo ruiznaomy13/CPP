5780a5afc107a6c243fb2c8e8d16a62a9cf7b7e2
Naomy Ruiz <109822565+ruiznaomy13@users.noreply.github.com>
2024-10-23 14:16:31 +0200

    metodos finalizados

--- CPP_07/ex00/hdr/Methods.hpp
+++ CPP_07/ex00/hdr/Methods.hpp
@@ -23,10 +23,19 @@ void swap(T& a, T& b)
 	b = c;
 }
 
-// template <class T>
-// void swap(T a, T b) {
-// 	T	 c = a;
-// 	a = b;
-// 	b = c;
-// }
+template <class T>
+T	*min(T& a, T& b)
+{
+	if (a >= b)
+		return (b);
+	return (a);
+}
+
+template <class T>
+T	*max(T& a, T& b)
+{
+	if (a <= b)
+		return (b);
+	return (a);
+}
 #endif
