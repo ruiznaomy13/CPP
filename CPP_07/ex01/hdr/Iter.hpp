9790114f3c3c566c627c9bfeb92a00d738b9e246
Nelly Naomy Castellanos Ruiz <ncastell@car8s1.42barcelona.com>
2024-10-24 13:11:04 +0200

    ok

--- CPP_07/ex01/hdr/Iter.hpp
+++ CPP_07/ex01/hdr/Iter.hpp
@@ -6,10 +6,20 @@
 /*   By: ncastell <ncastell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 19:59:10 by ncastell          #+#    #+#             */
-/*   Updated: 2024/10/23 20:00:17 by ncastell         ###   ########.fr       */
+/*   Updated: 2024/10/24 13:09:36 by ncastell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
-#define ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+
+template <class T>
+void	iter(T *array, int len, void (*fun)(T&))
+{
+	for (int i = 0; i < len; ++i)
+        fun(array[i]);
+}
+
 #endif
--- CPP_07/ex01/src/main.cpp
+++ CPP_07/ex01/src/main.cpp
@@ -6,7 +6,22 @@
 /*   By: ncastell <ncastell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 19:59:12 by ncastell          #+#    #+#             */
-/*   Updated: 2024/10/23 19:59:13 by ncastell         ###   ########.fr       */
+/*   Updated: 2024/10/24 13:10:31 by ncastell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Iter.hpp"
+
+void printInt(int& element) {
+	std::cout << element << " ";
+}
+
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5}; // Integer array
+
+	// Create an Iter object with the array and a function to apply to each element
+	iter(arr, 5, printInt);
+
+	return 0;
+}
