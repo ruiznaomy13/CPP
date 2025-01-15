f2fb7ec1688c45e2928b1346f2706989cd4bc9e6
Nelly Naomy Castellanos Ruiz <ncastell@cbr14s4.42barcelona.com>
2025-01-15 18:11:05 +0100

    empiezo ex01

---
+++ CPP_09/ex01/src/RPN.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncastell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 18:07:39 by ncastell          #+#    #+#             */
+/*   Updated: 2025/01/15 18:10:36 by ncastell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class	RPN
+{
+	private:
+		// TODO contenedor para los operadores
+
+	public:
+		// algoritmo para resolver el probblema
+
+	private:
+}
